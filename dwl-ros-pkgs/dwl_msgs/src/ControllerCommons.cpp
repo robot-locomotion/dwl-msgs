6f54dc129ad4338301c2e967d9dd0f422993fae9
Felipe Polido <felipe.polido@iit.it>
2016-05-25 20:49:23 +0200

    [feature] Changed the frequency initialization values

--- dwl-ros-pkgs/dwl_msgs/src/ControllerCommons.cpp
+++ dwl-ros-pkgs/dwl_msgs/src/ControllerCommons.cpp
@@ -6,7 +6,7 @@ namespace dwl_msgs
 
 ControllerCommons::ControllerCommons() : new_plan_(false), init_base_state_(false),
 		num_traj_points_(0), trajectory_counter_(0), controller_publish_rate_(50),
-		robot_publish_rate_(50), odom_publish_rate_(50), imu_publish_rate_(50),
+		robot_publish_rate_(250), odom_publish_rate_(250), imu_publish_rate_(250),
 		init_controller_state_pub_(false), init_robot_state_pub_(false),
 		init_odom_state_pub_(false), init_imu_state_pub_(false)
 {
