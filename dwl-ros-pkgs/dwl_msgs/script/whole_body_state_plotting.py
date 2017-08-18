90f7fbb0c66628648d557629bc8e8a782b10ab1e
Carlos Mastalli <carlos.mastalli@iit.it>
2017-08-18 19:10:18 +0200

    [python] Starting to create a plot script using the dwl python bindings

--- dwl-ros-pkgs/dwl_msgs/script/whole_body_state_plotting.py
+++ dwl-ros-pkgs/dwl_msgs/script/whole_body_state_plotting.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python
+import dwl
 import numpy as np
-
-from matplotlib.patches import Rectangle
-import matplotlib.pyplot as plt
-from matplotlib import rc
-rc('font',**{'family':'serif','serif':['Cardo']})
-rc('text', usetex=True)
-
+from dwl_msgs import BagParser as bag_parser
 import roslib; roslib.load_manifest('dwl_msgs')
 import argparse
-from dwl_msgs import BagParser as bag_parser
+import matplotlib.pyplot as plt
 
-
-    
+   
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''
@@ -22,22 +16,47 @@ if __name__ == '__main__':
     parser.add_argument('topic', help='Topic')
     args = parser.parse_args()
     
-    time, base_pos, base_vel, base_acc, joint_pos, joint_vel, joint_acc, contact_pos = bag_parser.extractWholeBodyState(args.bag, args.topic)
+    ws_vec = bag_parser.extractWholeBodyState(args.bag, args.topic)
+
+    time = []
+    base_pos_AX = []
+    base_pos_AY = []
+    base_pos_AZ = []
+    base_pos_LX = []
+    base_pos_LY = []
+    base_pos_LZ = []
+    for ws in ws_vec:
+        time.append(ws.getTime());
+        base_pos_AX.append(ws.getBaseRPY_W()[dwl.X]);
+        base_pos_AY.append(ws.getBaseRPY_W()[dwl.Y]);
+        base_pos_AZ.append(ws.getBaseRPY_W()[dwl.Z]);
+        base_pos_LX.append(ws.getBasePosition_W()[dwl.X]);
+        base_pos_LY.append(ws.getBasePosition_W()[dwl.Y]);
+        base_pos_LZ.append(ws.getBasePosition_W()[dwl.Z]);
+
+
+    #fig, ax = plt.subplots()
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111)
+#   ax.plot(time,base_pos[:,2*i+1], 'k', linewidth=2.5)
+    #ax.plot(np.array(time_vec),np.array(base_pos_x), 'r', linewidth=2.5)
+    ax.plot(time, base_pos_LX, 'r', linewidth=2.5)
+    plt.show()
 
     # Plotting the base states
-    num_base_joints = len(base_pos[0]) / 2
-    for i in range(num_base_joints):
-        # Plotting the base position
-        fig = plt.figure(i)
-        ax = fig.add_subplot(111)
-#        ax.plot(time,base_pos[:,2*i+1], 'k', linewidth=2.5)
-        ax.plot(time,base_pos[:,2*i], 'r', linewidth=2.5)
-        plt.title('Base Position', fontsize=18, fontweight='bold')
-        plt.ylabel('$z$ $[m]$', {'color':'k', 'fontsize':18})
-        plt.xlabel('$t$ $[s]$', {'color':'k', 'fontsize':18})
-        ax.grid(True)
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-        ax.yaxis.set_ticks_position('left')
-        ax.xaxis.set_ticks_position('bottom')
-        fig.savefig('base_pos_' + str(i) + '.pdf')
+#    num_base_joints = len(base_pos[0]) / 2
+#    for i in range(num_base_joints):
+#        # Plotting the base position
+#        fig = plt.figure(i)
+#        ax = fig.add_subplot(111)
+##        ax.plot(time,base_pos[:,2*i+1], 'k', linewidth=2.5)
+#        ax.plot(time,base_pos[:,2*i], 'r', linewidth=2.5)
+#        plt.title('Base Position', fontsize=18, fontweight='bold')
+#        plt.ylabel('$z$ $[m]$', {'color':'k', 'fontsize':18})
+#        plt.xlabel('$t$ $[s]$', {'color':'k', 'fontsize':18})
+#        ax.grid(True)
+#        ax.spines['right'].set_visible(False)
+#        ax.spines['top'].set_visible(False)
+#        ax.yaxis.set_ticks_position('left')
+#        ax.xaxis.set_ticks_position('bottom')
+#        fig.savefig('base_pos_' + str(i) + '.pdf')
--- dwl-ros-pkgs/dwl_msgs/src/dwl_msgs/BagParser.py
+++ dwl-ros-pkgs/dwl_msgs/src/dwl_msgs/BagParser.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 import roslib; roslib.load_manifest('dwl_msgs')
 
-import rosbag
+import dwl
 from dwl_msgs.msg import WholeBodyState
 import numpy as np
+import rosbag
+
 
 
 def extractWholeBodyState(bagfile, topic):
     print('Extracting the WholeBodyState message in topic' + topic +
           ' from rosbag ' + bagfile)
-    
+
     n = 0
     state_list = list()
     initial_time = 0
@@ -24,14 +26,7 @@ def extractWholeBodyState(bagfile, topic):
             ending_time = starting_time + duration
         
         # Recording the whole-body state trajectory
-        time_list = list()
-        base_pos_list = list()
-        base_vel_list = list()
-        base_acc_list = list()
-        joint_pos_list = list()
-        joint_vel_list = list()
-        joint_acc_list = list()
-        contact_list = list()
+        ws_list = []
         for (topic, msg, ts) in bag.read_messages(topics=str(topic)):
             # Recording the data in the desired time range
             if (ts.to_sec() >= starting_time and ts.to_sec() <= ending_time):
@@ -45,59 +40,80 @@ def extractWholeBodyState(bagfile, topic):
                     initial_time = ts.to_sec()
                 time = ts.to_sec() - initial_time
             
-                # Defining the state lists
-                base_pos = list()
-                base_vel = list()
-                base_acc = list()
-                joint_pos = list()
-                joint_vel = list()
-                joint_acc = list()
-                contact_state = list()
+
+                # Construct an instance of the WholeBodyState class, which wraps the C++ class.
+                ws = dwl.WholeBodyState()
+
+                # Setting up the DoF
+                ws.setJointDoF(num_joints)
+
+                # Getting the time
+                ws.setTime(time)
 
                 # Getting the base states
-                for i in range(num_base_joints):
-                    base_pos.append(msg.base[i].position)
-                    base_vel.append(msg.base[i].velocity)
-                    base_acc.append(msg.base[i].acceleration)
-                
+                base_pos = np.array([msg.base[dwl.LX].position,
+                                     msg.base[dwl.LY].position,
+                                     msg.base[dwl.LZ].position])
+                base_rpy = np.array([msg.base[dwl.AX].position,
+                                     msg.base[dwl.AY].position,
+                                     msg.base[dwl.AZ].position])
+                base_vel = np.array([msg.base[dwl.LX].velocity,
+                                     msg.base[dwl.LY].velocity,
+                                     msg.base[dwl.LZ].velocity])
+                base_omega = np.array([msg.base[dwl.AX].velocity,
+                                       msg.base[dwl.AY].velocity,
+                                       msg.base[dwl.AZ].velocity])
+                base_acc = np.array([msg.base[dwl.LX].velocity,
+                                     msg.base[dwl.LY].velocity,
+                                     msg.base[dwl.LZ].velocity])
+                base_omegad = np.array([msg.base[dwl.AX].velocity,
+                                        msg.base[dwl.AY].velocity,
+                                        msg.base[dwl.AZ].velocity])
+                ws.setBasePosition_W(base_pos)
+                ws.setBaseRPY_W(base_rpy)
+                ws.setBaseVelocity_W(base_vel)
+                ws.setBaseAngularVelocity_W(base_omega)
+                ws.setBaseAcceleration_W(base_acc)
+                ws.setBaseAngularAcceleration_W(base_omegad)
+
+
                 # Getting the joint states
                 for i in range(num_joints):
-                    joint_pos.append(msg.joints[i].position)
-                    joint_vel.append(msg.joints[i].velocity)
-                    joint_acc.append(msg.joints[i].acceleration)
-                                        
+                    ws.setJointPosition(msg.joints[i].position, i)
+                    ws.setJointVelocity(msg.joints[i].velocity, i)
+                    ws.setJointAcceleration(msg.joints[i].acceleration, i)
+                    ws.setJointEffort(msg.joints[i].effort, i)
+
                 # Getting the contact states
                 for i in range(num_contacts):
-                    contact_state.append(msg.contacts[i].position.x)
-                    contact_state.append(msg.contacts[i].position.y)
-                    contact_state.append(msg.contacts[i].position.z)
+                    name = msg.contacts[i].name
+                    contact_pos = np.array([msg.contacts[i].position.x,
+                                            msg.contacts[i].position.y,
+                                            msg.contacts[i].position.z])
+                    contact_vel = np.array([msg.contacts[i].velocity.x,
+                                            msg.contacts[i].velocity.y,
+                                            msg.contacts[i].velocity.z])
+                    contact_acc = np.array([msg.contacts[i].acceleration.x,
+                                            msg.contacts[i].acceleration.y,
+                                            msg.contacts[i].acceleration.z])
+                    contact_wrc = np.array([msg.contacts[i].wrench.torque.x,
+                                            msg.contacts[i].wrench.torque.y,
+                                            msg.contacts[i].wrench.torque.z,
+                                            msg.contacts[i].wrench.force.x,
+                                            msg.contacts[i].wrench.force.y,
+                                            msg.contacts[i].wrench.force.z])
+                    ws.setContactPosition_B(name, contact_pos)
+                    ws.setContactVelocity_B(name, contact_vel)
+                    ws.setContactAcceleration_B(name, contact_acc)
+                    ws.setContactWrench_B(name, contact_wrc)
 
-                base_pos_list.append(base_pos)
-                base_vel_list.append(base_vel)
-                base_acc_list.append(base_acc)
-                joint_pos_list.append(joint_pos)
-                joint_vel_list.append(joint_vel)
-                joint_acc_list.append(joint_acc)
-                contact_list.append(contact_state)
+                # Appeding the whole-state to the list
+                ws_list.append(ws)
 
                 n += 1
     print('Loaded ' + str(n) + ' whole-body controller states from the bagfile.')
     
-    # Getting the time info
-    for i in range(n):
-        time_list.append(i * duration/n)
-    
-    
-    time_vec = np.array(time_list)
-    base_pos_vec = np.array(base_pos_list)
-    base_vel_vec = np.array(base_vel_list)
-    base_acc_vec = np.array(base_acc_list)
-    joint_pos_vec = np.array(joint_pos_list)
-    joint_vel_vec = np.array(joint_vel_list)
-    joint_acc_vec = np.array(joint_acc_list)
-    contact_vec = np.array(contact_list)
-            
-    return time_vec, base_pos_vec, base_vel_vec, base_acc_vec, joint_pos_vec, joint_vel_vec, joint_acc_vec, contact_vec
+    return ws_list
 
 
 
