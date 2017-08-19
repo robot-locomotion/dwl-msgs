1656e106c34f38935fd945ad1956cbcba8119218
Carlos Mastalli <carlos.mastalli@iit.it>
2017-08-19 22:12:20 +0200

    [wip] Created the WholeBodyStateInterface in python

--- dwl-ros-pkgs/dwl_msgs/src/dwl_msgs/BagParser.py
+++ dwl-ros-pkgs/dwl_msgs/src/dwl_msgs/BagParser.py
@@ -2,7 +2,7 @@
 import roslib; roslib.load_manifest('dwl_msgs')
 
 import dwl
-from dwl_msgs.msg import WholeBodyState
+from dwl_msgs.msg import WholeBodyState, WholeBodyStateInterface
 import numpy as np
 import rosbag
 
@@ -135,86 +135,28 @@ def extractWholeBodyControllerState(bagfile, topic):
             ending_time = starting_time + duration
         
         # Recording the whole-body state trajectory
-        time_list = list()
-        base_pos_list = list()
-        base_vel_list = list()
-        base_acc_list = list()
-        joint_pos_list = list()
-        joint_vel_list = list()
-        joint_acc_list = list()
-        contact_list = list()
+        ws_actual_list = []
+        ws_desired_list = []
+        ws_errore_list = []
         for (topic, msg, ts) in bag.read_messages(topics=str(topic)):
             # Recording the data in the desired time range
             if (ts.to_sec() >= starting_time and ts.to_sec() <= ending_time):
-                # Setting the floating-base system DoF
-                num_base_joints = len(msg.actual.base)
-                num_joints = len(msg.actual.joints)
-                num_contacts = len(msg.actual.contacts)
-            
-                # Getting the current time
-                if (n == 0):
-                    initial_time = ts.to_sec()
-                time = ts.to_sec() - initial_time
-            
-                # Defining the state lists
-                base_pos = list()
-                base_vel = list()
-                base_acc = list()
-                joint_pos = list()
-                joint_vel = list()
-                joint_acc = list()
-                contact_state = list()
-
-                # Getting the base states
-                for i in range(num_base_joints):
-                    base_pos.append(msg.actual.base[i].position)
-                    base_pos.append(msg.desired.base[i].position)
-                    base_vel.append(msg.actual.base[i].velocity)
-                    base_vel.append(msg.desired.base[i].velocity)
-                    base_acc.append(msg.actual.base[i].acceleration)
-                    base_acc.append(msg.desired.base[i].acceleration)
+                # Defined the desired, actual and error whole-body states
+                ws_actual = dwl.WholeBodyState()
+                ws_desired = dwl.WholeBodyState()
+                ws_error = dwl.WholeBodyState()
                 
-                # Getting the joint states
-                for i in range(num_joints):
-                    joint_pos.append(msg.actual.joints[i].position)
-                    joint_pos.append(msg.desired.joints[i].position)
-                    joint_vel.append(msg.actual.joints[i].velocity)
-                    joint_vel.append(msg.desired.joints[i].velocity)
-                    joint_acc.append(msg.actual.joints[i].acceleration)
-                    joint_acc.append(msg.desired.joints[i].acceleration)
-                                        
-                # Getting the contact states
-                for i in range(num_contacts):
-                    contact_state.append(msg.actual.contacts[i].position.x)
-                    contact_state.append(msg.actual.contacts[i].position.y)
-                    contact_state.append(msg.actual.contacts[i].position.z)
-                    contact_state.append(msg.desired.contacts[i].position.x)
-                    contact_state.append(msg.desired.contacts[i].position.y)
-                    contact_state.append(msg.desired.contacts[i].position.z)
-
-                base_pos_list.append(base_pos)
-                base_vel_list.append(base_vel)
-                base_acc_list.append(base_acc)
-                joint_pos_list.append(joint_pos)
-                joint_vel_list.append(joint_vel)
-                joint_acc_list.append(joint_acc)
-                contact_list.append(contact_state)
+                wbi = WholeBodyStateInterface()
+                wbi.writeFromMessage(ws_actual, msg.actual)
+                wbi.writeFromMessage(ws_desired, msg.desired)
+                wbi.writeFromMessage(ws_error, msg.error)
+                
+                # Appeding the whole-state to the list
+                ws_actual_list.append(ws_actual)
+                ws_desired_list.append(ws_desired)
+                ws_error_list.append(ws_error)
 
                 n += 1
     print('Loaded ' + str(n) + ' whole-body controller states from the bagfile.')
     
-    # Getting the time info
-    for i in range(n):
-        time_list.append(i * duration/n)
-    
-    
-    time_vec = np.array(time_list)
-    base_pos_vec = np.array(base_pos_list)
-    base_vel_vec = np.array(base_vel_list)
-    base_acc_vec = np.array(base_acc_list)
-    joint_pos_vec = np.array(joint_pos_list)
-    joint_vel_vec = np.array(joint_vel_list)
-    joint_acc_vec = np.array(joint_acc_list)
-    contact_vec = np.array(contact_list)
-
-    return time_vec, base_pos_vec, base_vel_vec, base_acc_vec, joint_pos_vec, joint_vel_vec, joint_acc_vec, contact_vec
+    return ws_actual_list, ws_desired_list, ws_error_list
